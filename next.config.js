086e8a250ca10f197643485f7b1ee87d92e4bd5e
Maxime <maximedebuyn@gmail.com>
2025-06-14 22:30:28 +0200

    vercel

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/podcast-transcription-app' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/podcast-transcription-app' : '',
 }
 
 module.exports = nextConfig
